ac9cde82d48766768ec8edc82f715a402bff1a61
Angel Lim <toomanytypos@gmail.com>
2016-07-23 20:08:36 -0700

    http get on google

--- go-http-server.go
+++ go-http-server.go
@@ -3,13 +3,21 @@ package main
 import (
 	"io"
 	"net/http"
+	"fmt"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world!")
+	io.WriteString(w, "Hello world!\n\n")
+	resp, err := http.Get("http://www.google.com")
+	io.WriteString(w, (*resp).Status)
+	fmt.Println(err)
+
 }
 
 func main() {
+
+
+	
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":8000", nil)
 }
